e9a9285b305b362f05d5c7f342b33f7d1a0602d7
SPJ1999 <surajjaiswal748@gmail.com>
2025-08-19 11:41:43 +0000

    Variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12345;
+let accountEmail="surajjaiswal748@gmail.com"
+const accountPassword=4567677;
+const accountCity="new york"
+// accountId =4;
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table({ accountId, accountEmail, accountPassword, accountCity });
+
